8c2614ec7eef6a4f2c0959c5c1d83bfa171787c7
AbstractUmbra <Umbra@AbstractUmbra.xyz>
2020-10-01 11:17:08 +0100

    begin real 0.1.0

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ A small simple wrapper around the [Mystb.in](https://mystb.in/) API.
 
 ### Features
 
-[x] - `POST`ing to the API, which will return the provided url.\
-[x] - `GET`ting from the API, provided you know the URL or ID. \
-[ ] - Authorization. Awaiting the API making this public as it is still WIP.
+[x] - `POST`ing to the API, which will return the provided url. \
+[x] - `GET`ting from the API, provided you know the URL or paste ID. \
+[ ] - Ability to pass in a sync or async session / parameter so it is flexible.
+
+[ ] - Write a real underlying Client for this, it will be required for... \
+[ ] - Authorization. Awaiting the API making this public as it is still WIP. \
+
+### Dependencies
+
+`aiohttp` - required
+`requests` - optional
--- mystbin/__init__.py
+++ mystbin/__init__.py
@@ -21,37 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
-import re
 
-import aiohttp
-
-BASE = "https://mystb.in/api/pastes"
-# Let's set aiohttp timeout for this
-TIMEOUT = aiohttp.ClientTimeout(total=15.0)
-
-# grab the paste id: https://regex101.com/r/qkluDh/1
-MB_URL_RE = re.compile(r"(?:http[s]?://mystb\.in/)?([a-zA-Z]+)(?:.*)")
-
-async def post(content: str, *, session: aiohttp.ClientSession = None, suffix: str = None) -> str:
-    """ Post `content` to MystB.in with optional suffix text. """
-    suffix = f".{suffix}" if suffix else ""
-    multi_part = aiohttp.MultipartWriter()
-    paste_content = multi_part.append(content)
-    paste_content.set_content_disposition("form-data", name="data")
-    paste_content = multi_part.append_json({"meta": [{"index": 0, "syntax": suffix}]})
-    paste_content.set_content_disposition("form-data", name="meta")
-    session = session or aiohttp.ClientSession(raise_for_status=True)
-    async with session.post(BASE, data=multi_part, timeout=TIMEOUT) as mb_res:
-        url = await mb_res.json()
-    short_id = url['pastes'][0]['id']
-    return f"https://mystb.in/{short_id}{suffix}"
-
-async def get(paste: str, *, session: aiohttp.ClientSession = None) -> dict:
-    try:
-        paste_id = MB_URL_RE.match(paste)[1]
-    except IndexError:
-        return "This is not a valid Mystb.in paste ID or URL."
-    session = session or aiohttp.ClientSession(raise_for_status=True)
-    async with session.get(f"{BASE}/{paste_id}", timeout=TIMEOUT) as mb_res:
-        data_json = await mb_res.json()
-    return data_json
+from .client import MystClient
+from .errors import *
