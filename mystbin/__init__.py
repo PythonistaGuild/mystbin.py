09598801ee69b962e675d8b188d498abd8d589ab
AbstractUmbra <Umbra@AbstractUmbra.xyz>
2020-10-06 13:49:56 +0100

    v0.2.5 - fixed optional dependency issue, and init edited, with version bump

--- mystbin/__init__.py
+++ mystbin/__init__.py
@@ -22,5 +22,5 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
-from .client import MystClient
+from .client import MystbinClient
 from .errors import *
