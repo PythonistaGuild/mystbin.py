98bd716412da2cb3e461a3e53dc47710dd261925
AbstractUmbra <Umbra@AbstractUmbra.xyz>
2020-10-09 20:42:15 +0100

    add objects documentation and corrected typing

--- mystbin/objects.py
+++ mystbin/objects.py
@@ -28,31 +28,68 @@ from textwrap import dedent
 from .constants import PASTE_BASE
 
 class Paste:
+    """
+    A class representing the return data from the API after performing a POST request.
+
+    Parameters
+    ----------
+    paste_id: :class:`str`
+        The ID returned from the API. Genertally it is 3 random choice English words.
+    nick: :class:`str`
+        The nickname you requested the paste be named.
+    syntax: :class:`str`
+        The syntax (or syntax highlighting) you requested when creating the Paste. Returns as a suffix on the URL.
+    """
+
     __slots__ = ("paste_id", "nick", "syntax")
-    def __init__(self, json_data: dict, syntax: str = None) -> None:
+
+    def __init__(self, json_data: dict, syntax: str = None):
         self.paste_id = json_data['pastes'][0]['id']
         self.nick = json_data['pastes'][0]['nick']
         self.syntax = syntax
 
     def __str__(self) -> str:
-        """ Cast the Paste to a string for the URL. """
         return self.url
 
     def __repr__(self) -> str:
-        """ Paste repr. """
         return f"<Paste id={self.paste_id} nick={self.nick} syntax={self.syntax}>"
 
     @property
     def url(self) -> str:
+        """ :class:`str`: Returns the formatted url of ID and syntax. """
         syntax = f".{self.syntax}" if self.syntax else ""
         return PASTE_BASE.format(self.paste_id, syntax)
-    
+
     def with_syntax(self, new_syntax: str) -> str:
+        """
+        Changes the syntax of the current Paste to `new_syntax`
+
+        Parameters
+        ----------
+        new_syntax: :class:`str`
+            The new suffix to append to the Paste.
+        """
         return PASTE_BASE.format(self.paste_id, new_syntax)
 
 class PasteData:
+    """
+    A class representing the return data from the API after performing a GET request.
+
+    Parameters
+    ----------
+    paste_id: :class:`str`
+        The ID you wish to retrieve from the API.
+    paste_content: :class:`str`
+        The content returned from the paste.
+    paste_syntax: :class:`str`
+        The syntax you specified that this Paste is in.
+    paste_nick: :class:`str`
+        The nick set for this paste on the API.
+    paste_date: :class:`datetime.datetime`
+        The date this paste was created on the API.
+    """
     __slots__ = ("paste_id", "_paste_data", "paste_content", "paste_syntax", "paste_nick", "paste_date")
-    def __init__(self, paste_id: str, paste_data: dict) -> None:
+    def __init__(self, paste_id: str, paste_data: dict):
         self.paste_id = paste_id
         self._paste_data = paste_data
         self.paste_content = paste_data['data']
@@ -61,26 +98,24 @@ class PasteData:
         self.paste_date = paste_data['created_at']
 
     def __str__(self) -> str:
-        """ We'll return the paste content. Since it's dev stuff, dedent it. """
         return self.content
 
     def __repr__(self) -> str:
-        """ Paste content repr. """
         return f"<PasteData id={self.paste_id} nick={self.paste_nick} syntax={self.paste_syntax}>"
 
     @property
     def url(self) -> str:
-        """ The Paste ID's URL """
+        """ :class:`str`: The Paste ID's URL """
         syntax = f".{self.paste_syntax}" if self.paste_syntax else ""
         return PASTE_BASE.format(self.paste_id, syntax)
 
     @property
     def created_at(self) -> datetime.datetime:
-        """ Returns a UTC datetime of when the paste was created. """
+        """ :class:`datetime.datetime`: Returns a UTC datetime of when the paste was created. """
         return datetime.datetime.strptime(self.paste_date, "%Y-%m-%dT%H:%M:%S.%f")
 
     @property
     def content(self) -> str:
-        """ Return the paste content but dedented correctly. """
+        """ :class:`str`: Return the paste content but dedented correctly. """
         return dedent(self.paste_content)
-    
+
