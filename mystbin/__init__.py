875bd1fa01b8ecd63b47741b1d818db2e65fdb32
Alex Nørgaard <Umbra@AbstractUmbra.xyz>
2020-11-26 12:31:47 +0000

    version bump

--- mystbin/__init__.py
+++ mystbin/__init__.py
@@ -27,8 +27,8 @@ from collections import namedtuple
 from .client import Client
 from .errors import *
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 VersionInfo = namedtuple(
     "VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(major=1, minor=0, micro=0,
+version_info = VersionInfo(major=1, minor=0, micro=1,
                            releaselevel='final', serial=0)
