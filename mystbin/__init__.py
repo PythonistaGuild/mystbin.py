ebf2a37673b5f8057ad6564de0abf3f21867ce8b
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2022-08-30 00:43:44 +0100

    version bump

--- mystbin/__init__.py
+++ mystbin/__init__.py
@@ -22,7 +22,7 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
-__version__ = "5.0.0a"
+__version__ = "5.0.1"
 
 from typing import Literal, NamedTuple
 
@@ -40,4 +40,4 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info: VersionInfo = VersionInfo(major=5, minor=0, micro=0, releaselevel="alpha", serial=0)
+version_info: VersionInfo = VersionInfo(major=5, minor=0, micro=1, releaselevel="final", serial=0)
